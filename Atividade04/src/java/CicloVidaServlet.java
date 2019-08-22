b8c48321318a62d5d87555eb3cc5d7b055823dcf
19214290010 <19214290010@OLAB04-D27.academico.iesb.br>
2019-08-21 21:05:34 -0300

    Atividade 04 Concluida

--- Atividade04/src/java/CicloVidaServlet.java
+++ Atividade04/src/java/CicloVidaServlet.java
@@ -1,6 +1,7 @@
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,36 +11,31 @@ import javax.servlet.http.HttpServletResponse;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author 19214290010
  */
-public class CicloVidaServlet extends HttpServlet{
+@WebServlet(name = "CicloVidaServlet", urlPatterns = "/CicloVidaServlet")
+
+public class CicloVidaServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
-        
+
         System.out.println("Executando CicloVidaServlet");
-       
     }
 
     @Override
     public void init() throws ServletException {
-        super.init(); //To change body of generated methods, choose Tools | Templates.
-        
+
         System.out.println("Iniciando CicloVidaServlet.init()");
     }
 
     @Override
     public void destroy() {
-        super.destroy(); //To change body of generated methods, choose Tools | Templates.
-        
-        System.out.println ("Finalizando CicloVidaServlet.detroy ()");
-        
+
+        System.out.println("Finalizando CicloVidaServlet.detroy ()");
+
     }
-    
-    
-    
+
 }
