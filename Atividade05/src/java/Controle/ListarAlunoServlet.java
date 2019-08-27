7e431f4b52ba684c850509d9c0e0fb0fea81e0fa
19214290010 <19214290010@OLAB04-D27.academico.iesb.br>
2019-08-27 20:53:45 -0300

    Criando Atividade05

--- Atividade05/src/java/Controle/ListarAlunoServlet.java
+++ Atividade05/src/java/Controle/ListarAlunoServlet.java
@@ -9,11 +9,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.AlunoBO;
+import modelo.NegocioException;
 import modelo.entidades.Aluno;
 
 /**
@@ -22,37 +26,8 @@ import modelo.entidades.Aluno;
  */
 @WebServlet(name = "ListarAlunoServlet", urlPatterns = {"/aluno"})
 public class ListarAlunoServlet extends HttpServlet {
-    
-     List<Aluno> aluno = new ArrayList<Aluno>();
-    
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ListarAlunoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ListarAlunoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -64,6 +39,21 @@ public class ListarAlunoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        List<Aluno> aluno = new ArrayList<Aluno>();
+
+        AlunoBO bo = new AlunoBO();
+        try {
+            bo.listar();
+        } catch (NegocioException ex) {
+            Logger.getLogger(ListarAlunoServlet.class.getName()).log(Level.SEVERE, null, ex);
+            
+            PrintWriter out = response.getWriter();
+
+        }
+
     }
 
     /**
